67830a5eb89abb3da919a4e69946207208499d5b
blong145 <blong145@gmail.com>
2017-02-18 10:32:56 -0600

    Initial check-in of module HackerRank

---
+++ HackerRank.Tests/InsertionSortPart1.cs/InsertionSortPart1Tests.cs
@@ -0,0 +1,89 @@
+﻿using NUnit.Framework;
+using System;
+namespace HackerRank.Tests
+{
+	[TestFixture()]
+	public class WhenShiftingRight
+	{
+		[Test()]
+		public void ShouldCopyTheSecondToLastElementToTheLastPlace()
+		{
+			//arrange
+			var arranged = new[] { 2, 4, 6, 8, 3 };
+			//act
+			var result = Solution.ShiftRight(4, arranged);
+
+			//assert
+			CollectionAssert.AreEqual(new[] { 2, 4, 6, 8, 8 }, result);
+		}
+
+		[Test()]
+		public void ShouldCopyTheThirdToLastElementToTheSecondToLastPlace()
+		{
+			//arrange
+			var insertionSortPart1 = new Solution();
+			var arranged = new[] { 2, 4, 6, 8, 8 };
+			//act
+			var result = Solution.ShiftRight(3, arranged);
+
+			//assert
+			CollectionAssert.AreEqual(new[] { 2, 4, 6, 6, 8 }, result);
+		}
+
+		[Test()]
+		public void ShouldDoNothingForTheFirstIndex()
+		{
+			//arrange
+			var insertionSortPart1 = new Solution();
+			var arranged = new[] { 2, 3, 4, 6, 8 };
+			//act
+			var result = Solution.ShiftRight(0, arranged);
+
+			//assert
+			CollectionAssert.AreEqual(new[] { 2, 3, 4, 6, 8 }, result);
+		}
+	}
+
+	[TestFixture]
+	public class WhenPerformingAnInsertionSort
+	{
+		[Test]
+		public void ShouldReturnASortedListWhenTheUnorderedNumberExistsInTheMiddle()
+		{
+			//arrange
+			var insertionSortPart1 = new Solution();
+			var arranged = new[] { 2, 4, 6, 8, 3 };
+			//act
+			var result = Solution.ActualInsertionSort(arranged);
+
+			//assert
+			CollectionAssert.AreEqual(new[] { 2, 3, 4, 6, 8 }, result);
+		}
+
+		[Test]
+		public void ShouldReturnASortedListWhenTheUnorderedNumberShouldBeFirst()
+		{
+			//arrange
+			var insertionSortPart1 = new Solution();
+			var arranged = new[] { 3, 4, 6, 8, 1 };
+			//act
+			var result = Solution.ActualInsertionSort(arranged);
+
+			//assert
+			CollectionAssert.AreEqual(new[] { 1, 3, 4, 6, 8 }, result);
+		}
+
+		[Test]
+		public void ShouldReturnASortedListWhenTheUnorderedNumberIsLast()
+		{
+			//arrange
+			var insertionSortPart1 = new Solution();
+			var arranged = new[] { 3, 4, 6, 8, 9 };
+			//act
+			var result = Solution.ActualInsertionSort(arranged);
+
+			//assert
+			CollectionAssert.AreEqual(new[] {3, 4, 6, 8, 9 }, result);
+		}
+	}
+}
---
+++ HackerRank/InsertionSortPart1/InsertionSortPart1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace HackerRank
+{
+	// https://www.hackerrank.com/challenges/insertionsort1
+	public class Solution
+	{
+		static void Main(String[] args)
+		{
+			int _ar_size;
+			_ar_size = Convert.ToInt32(Console.ReadLine());
+			int[] _ar = new int[_ar_size];
+			String elements = Console.ReadLine();
+			String[] split_elements = elements.Split(' ');
+			for (int _ar_i = 0; _ar_i < _ar_size; _ar_i++)
+			{
+				_ar[_ar_i] = Convert.ToInt32(split_elements[_ar_i]);
+			}
+
+			insertionSort(_ar);
+		}
+
+		public static void insertionSort(int[] ar)
+		{
+			ActualInsertionSort(ar);
+			Console.WriteLine(string.Join(" ", ar));
+		}
+
+		public static int[] ActualInsertionSort(int[] input)
+		{
+			var unsortedValue = input[input.Length - 1];
+			var sortedArray = input;
+			for (int i = input.Length - 1; i >= 0; i--)
+			{
+				sortedArray = ShiftRight(i, sortedArray);
+
+				if (unsortedValue > sortedArray[i] || i == 0)
+				{
+					sortedArray[i] = unsortedValue;
+					return sortedArray;
+				}
+
+				Console.WriteLine(string.Join(" ", sortedArray));
+			}
+
+			return sortedArray;
+		}
+
+		public static int[] ShiftRight(int index, int[] input)
+		{
+			var shiftedArray = input;
+
+			if (index > 0)
+			{
+				shiftedArray[index] = shiftedArray[index - 1];
+			}
+			return shiftedArray;
+		}
+	}
+}
+
+
